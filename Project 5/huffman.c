917cf896807e237c76c76ff65fec22f0ba078055
svenkatesh1 <47004658+svenkatesh1@users.noreply.github.com>
2019-03-30 09:37:37 +0530

    Add files via upload

---
+++ Project 5/huffman.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include "pqueue.h"
+#include "pack.h"
+#define END 256
+
+//Declares NODE as struct node
+typedef struct node NODE;
+
+NODE *makeNode(NODE *left, NODE *right)
+{
+    NODE *rt;
+    rt = malloc(sizeof(NODE*));
+    assert(rt != NULL);
+    rt->count = left->count + right->count;
+    rt->parent = NULL;
+    left->parent = rt;
+    right->parent = rt;
+    return rt;
+}
+
+//Function compartes the data in two nodes
+//Big-O: O(1)
+int compare(NODE *first, NODE *second)
+{
+    int fst1 = first->count;
+    int scnd2 = second->count;
+    return (fst1 < scnd2) ? -1 : (fst1 > scnd2);
+}
+
+
+int main(int argc, char *argv[])
+{
+    NODE *pNode = malloc(sizeof(NODE *));;
+    NODE *pNode1 = malloc(sizeof(NODE *));;
+    FILE *fp;
+    fp=fopen(argv[1],"r");
+    PQ *pq;
+
+    int i;
+    int arr1[END+1];
+    NODE *arr2[END + 1];
+    int root_tot=0;
+    int pn,pn_1;
+    for(i=0;i<END+1;i++) //O(1) becuse it will always run 256 times
+    {
+        arr1[i]=0;
+        arr2[i]=NULL;
+    }
+    i=0;
+    while(i!=EOF) //O(n)
+    {
+        i=getc(fp);
+        arr1[i]+=1;
+    }
+    fclose(fp);
+    pq = createQueue(compare);
+    NODE *newn = malloc(sizeof(NODE));
+    newn->count=0;
+    newn->parent=NULL;
+    addEntry(pq,newn);
+    for(i=0;i<END+1;i++) //O(1)
+    {
+        arr2[256]=malloc(sizeof(NODE *));
+        arr2[256]=newn;
+        arr1[256]=0;
+        
+        if(arr1[i]>0)
+        {
+            arr2[i]=malloc(sizeof(NODE *));
+            assert(arr2[i]!=NULL);
+            arr2[i]->count=arr1[i];
+            arr2[i]->parent=NULL;
+            addEntry(pq,arr2[i]);
+            root_tot+=arr1[i];
+        }
+        addEntry(pq,arr2[256]);
+    }
+    pn=-1;
+    pn_1=-1;
+    NODE* rootnode;
+    rootnode=malloc(sizeof(NODE*));
+    while(numEntries(pq)>1)
+    {
+        pNode=removeEntry(pq);
+        pNode1=removeEntry(pq);
+        rootnode=makeNode(pNode,pNode1);
+        rootnode->parent=NULL;
+        rootnode->count=((pNode->count)+(pNode1->count));
+        pNode->parent=rootnode;
+        pNode1->parent=rootnode;
+        addEntry(pq,rootnode);
+        
+    }
+    
+    pn=0;
+    pn_1=0;
+    NODE *pNode3;
+    pNode3=malloc(sizeof(NODE*));
+    
+    for(i=0;i<END;i++)
+    {
+        if(arr1[i]>0)
+        {
+            pn++;
+            pNode3=arr2[i];
+            for(pn_1=0;pNode3->parent!=NULL;pn_1++)
+            {
+                pNode3=pNode3->parent;
+            }
+            printf("\n %c %d x %d bits equals %d bits\n",i,arr2[i]->count,pn_1,(pn_1)*(arr2[i]->count));
+            
+        }
+    }
+    printf("number of nonnull %d\n",pn);
+    pack(argv[1],argv[2],arr2);
+    destroyQueue(pq);
+    exit(EXIT_SUCCESS);
+}
---
+++ Project 5/pqueue.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "pqueue.h"
+
+//Defined constant and macros
+#define START 10
+#define p(x) (((x)-1)/2)
+#define l(x) (((x)*2)+1)
+#define r(x) (((x)*2)+2)
+
+typedef struct pqueue
+{
+    int count;
+    int length;
+    void **data;
+    int (*compare) ();
+}PQ;
+
+//Function creates and initializes queue
+//O(1)
+PQ *createQueue(int (*compare)())
+{
+    PQ *pq;
+    pq = malloc(sizeof(PQ));
+    assert(pq != NULL);
+    pq->count = 0;
+    pq->length = START;
+    pq->compare = compare;
+    assert(compare != NULL);
+    pq->data = malloc(sizeof(void*)*(pq->length));
+    return pq;
+}
+
+//Function destroys queue by freeing from memory
+//O(1)
+void destroyQueue(PQ *pq)
+{
+    assert(pq != NULL);
+    free(pq->data);
+    free(pq);
+}
+
+//Function returns the number of nodes
+//Big-O: O(1)
+int numEntries(PQ *pq)
+{
+    assert(pq != NULL);
+    return pq->count;
+}
+
+//Function adds a node to the priority queue and reallocates memory to 2x size if full to limit expensive reallocation
+//O(log n)
+void addEntry(PQ *pq, void *entry)
+{
+    assert(pq != NULL && entry != NULL);
+    if(pq->length == pq->count)
+    {
+        pq->length *= 2;
+        pq->data = realloc(pq->data, sizeof(void *)*pq->length*2);
+    }
+    pq->data[pq->count] = entry;
+    int location = pq->count;
+    while(pq->compare(pq->data[location], pq->data[p(location)]) < 0)
+    {
+        void *temp = pq->data[p(location)];
+        pq->data[p(location)] = pq->data[location];
+        pq->data[location] = temp;
+        location = p(location);
+    }
+    pq->count++;
+}
+
+//Function returns the deleted element and reorganizes queue as neccessary
+//O(log n)
+void *removeEntry(PQ *pq)
+{
+    assert(pq != NULL);
+    void *root = pq->data[0];
+    int location, small;
+    location = 0;
+    small = 0;
+    pq->data[location] = pq->data[pq->count - 1];
+    pq->count--;
+    while(l(location) < pq->count)
+    {
+        small = l(location);
+        if(r(location) < pq->count)
+        {
+            if(pq->compare(pq->data[l(location)] , pq->data[r(location)]) < 0)
+                small = l(location);
+            else
+                small = r(location);
+        }
+        if(pq->compare(pq->data[small],pq->data[location]) < 0)
+        {
+            void *temp = pq->data[small];
+            pq->data[small] = pq->data[location];
+            pq->data[location] = temp;
+            location = small;
+        }
+        else
+            break;
+    }
+    return root;
+}
